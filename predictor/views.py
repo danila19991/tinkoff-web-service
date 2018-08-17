6b28f4590d2f080a315d629823721f3f22b9c1b9
Данила <guseffdanila@ya.ru>
2018-08-17 17:54:08 +0300

    Adding moc-logic for processing prediction ewb page.

--- predictor/views.py
+++ predictor/views.py
@@ -6,17 +6,20 @@ from django.http import FileResponse
 
 def index(request):
     if request.method == 'POST':
-        with open('data.cvs', 'wb+') as f:
+        result = ''
+        if 'menu' in request.FILES and 'file' in request.FILES:
             for chunk in request.FILES['menu'].chunks():
-                f.write(chunk)
-            f.write('\n'.encode())
+                result += chunk.decode()
+            result += ' '
             for chunk in request.FILES['file'].chunks():
-                f.write(chunk)
+                result += chunk.decode()
+        request.session['result'] = result
+        return render(request, 'predictor/index.html', {'some_text': result})
+    if request.method == 'GET' and 'result' in request.session:
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="predictions.csv'
+        response['Content-Disposition'] = \
+            'attachment; filename="predictions.csv'
         writer = csv.writer(response)
-        writer.writerow(['First row', 'Foo', 'Bar', 'Baz'])
-        writer.writerow(
-            ['Second row', 'A', 'B', 'C', '"Testing"', "Here's a quote"])
+        writer.writerow(request.session['result'])
         return response
     return render(request, 'predictor/index.html', {})
