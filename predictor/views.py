3b117fb4143c10dbc0f9b41f20645b475e85971c
Данила <guseffdanila@ya.ru>
2018-08-20 23:21:14 +0300

    Add user authorisation. closes#2 closes#3 closes#9

--- predictor/views.py
+++ predictor/views.py
@@ -1,15 +1,28 @@
 import csv
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from predictor.views_utils import make_prediction
+from predictor.views_utils import check_content
+from predictor.views_utils import validate_email
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
+@login_required(login_url='/auth')
 def index(request):
-    if request.method == 'POST' and 'file' in request.FILES:
-        request.session['result'] = make_prediction(request.FILES['file'])
+    if request.method == 'POST' and 'input_data' in request.FILES:
+        try:
+            request.session['result'] = \
+                make_prediction(request.FILES['input_data'])
+        except:
+            return render(request, 'predictor/index.html',
+                          {'invalid_data': True})
+
+    if request.method == 'GET' and 'logout' in request.GET:
+        logout(request)
+        return HttpResponseRedirect(reverse('predictor:index'))
 
     if request.method == 'GET' and 'download' in request.GET and\
             'result' in request.session:
@@ -32,20 +45,55 @@ def index(request):
                 break
 
         return render(request, 'predictor/index.html',
-                      {'some_text': str_result})
+                      {'result_description': str_result})
     else:
         return render(request, 'predictor/index.html', {})
 
 
 def auth(request):
-    if request.method == 'GET' and 'submit' in request.GET:
-        return HttpResponseRedirect(reverse('predictor:index'))
+    if request.method == 'POST' and 'submit' in request.POST:
+        necessary_fields = ('username', 'password')
+
+        error_context = check_content(necessary_fields, request.POST)
+
+        if error_context:
+            return render(request, 'predictor/auth.html', error_context)
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if 'next' in request.GET:
+                return HttpResponseRedirect(request.GET['next'])
+            else:
+                return HttpResponseRedirect(reverse('predictor:index'))
+        else:
+            return render(request, 'predictor/auth.html',
+                          {'wrong_username_or_password': True})
 
     return render(request, 'predictor/auth.html', {})
 
 
 def register_page(request):
-    if request.method == 'GET' and 'register' in request.GET:
+    if request.method == 'POST':
+        necessary_fields = ('first_name', 'last_name', 'email', 'password')
+
+        error_context = check_content(necessary_fields, request.POST)
+
+        if not error_context:
+            if User.objects.filter(username=request.POST['email']):
+                error_context['another_name'] = True
+            if validate_email(request.POST['email']):
+                error_context['another_email'] = True
+
+        if error_context:
+            return render(request, 'predictor/register.html', error_context)
+
+        User.objects.create_user(request.POST['email'], request.POST['email'],
+                                 request.POST['password'],
+                                 first_name=request.POST['first_name'],
+                                 last_name=request.POST['last_name'])
         return HttpResponseRedirect(reverse('predictor:auth'))
     return render(request, 'predictor/register.html', {})
 
@@ -57,5 +105,10 @@ def restore(request):
     return render(request, 'predictor/restore.html', {})
 
 
+@login_required(login_url='/auth')
 def research_page(request):
+    if request.method == 'GET' and 'logout' in request.GET:
+        logout(request)
+        return HttpResponseRedirect(reverse('predictor:research'))
+
     return render(request, 'predictor/research.html', {})
