b7f7947a9f7d83da27300e58174656f94421474f
Данила <guseffdanila@ya.ru>
2018-08-27 11:17:10 +0300

    Add generating secret key during settings setup.

--- settings_installation.py
+++ settings_installation.py
@@ -1,6 +1,7 @@
 from mlalgorithms.shell import Shell
 import os
 import shutil
+import random
 
 
 # Delete migrations.
@@ -26,6 +27,14 @@ os.system('python manage.py migrate')
 if not os.path.exists('models'):
     print('add models storage')
     os.mkdir('models')
+
+with open('secret_key.txt','w+') as f:
+    chars = 'qwertyuiopasdfghjkzxcvbnmlQWERTYUIOPASDFGHJKLZXCVBNM,.' \
+            '/;[]<>?:"{}!@#$%^&*()_+1234567890-='
+    for i in range(50):
+        f.write(chars[random.randint(0, len(chars)-1)])
+
+
 
 
 sh = Shell()
