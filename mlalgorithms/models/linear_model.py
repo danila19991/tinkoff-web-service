d7ad59ca04a69c639e20717d67bfa3dcf5a7d227
Данила <guseffdanila@ya.ru>
2018-08-29 15:55:27 +0300

    Update ml_algorithm

--- mlalgorithms/models/linear_model.py
+++ mlalgorithms/models/linear_model.py
@@ -1,29 +1,16 @@
-import numpy as np
-
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 
 from . import model
 
 
-class LinearModel(model.IModel):
+class LinearModel(model.SimpleModel):
 
     def __init__(self, **kwargs):
-        self.model = LinearRegression(**kwargs)
-
-    def train(self, train_samples, train_labels, **kwargs):
-        self.model.fit(train_samples, train_labels, **kwargs)
-
-    def predict(self, samples, **kwargs):
-        predictions = []
-        for sample in samples:
-            prediction = self.model.predict(np.array(sample).reshape(1, -1))[0]
-            predictions.append(prediction)
-        return predictions
+        super().__init__(LinearRegression(**kwargs))
 
 
-class RidgeModel(LinearModel):
+class RidgeModel(model.SimpleModel):
 
     def __init__(self, **kwargs):
-        super().__init__()
-        self.model = Ridge(**kwargs)
+        super().__init__(Ridge(**kwargs))
